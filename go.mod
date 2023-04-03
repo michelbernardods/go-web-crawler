d2547ae4d47d9df6fe34553f934e85258cbd94de
Michel Bernardo <michel_13s@outlook.com>
2023-04-03 00:45:22 -0300

    feat: listing page links

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/michelbernardods/go-web-crawler
 
-go 1.20
+go 1.19
+
+require golang.org/x/net v0.8.0
--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/net/html"
+)
+
+var links []string
 
 func main() {
-	fmt.Println("web crawler")
+	url := "https://github.com/michelbernardods?tab=repositories"
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("status not is 200: %d", res.StatusCode))
+	}
+
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	extractLinks(doc)
+	fmt.Println(len(links), "links")
+}
+
+func extractLinks(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, attr := range n.Attr {
+			if attr.Key != "href" {
+				continue
+			}
+
+			link, err := url.Parse(attr.Val)
+			if err != nil || link.Scheme == "" {
+				continue
+			}
+
+			links = append(links, link.String())
+			fmt.Println(link)
+		}
+
+	}
+
+	for nav := n.FirstChild; nav != nil; nav = nav.NextSibling {
+		extractLinks(nav)
+	}
 }
